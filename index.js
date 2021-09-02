0d20700fefa11b743dbd2feffc2763ea772e0eec
Hbrown16 <hbrown1094@gmail.com>
2021-09-02 19:23:15 -0400

    connection array

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer');
+const { start } = require('repl');
 const connection = require('./config/connect');
 
 
 connection.connect((err) => {
-    
-})
+    if (err) throw err;
+    start();
+});

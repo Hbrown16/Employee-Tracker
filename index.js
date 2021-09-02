0a4aeadca45e5523e8ad52e2edfb7b50c2eedd06
Hbrown16 <hbrown1094@gmail.com>
2021-09-02 19:26:20 -0400

    start prompt

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ connection.connect((err) => {
     if (err) throw err;
     start();
 });
+
+const start = () => {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'Welcome what would you like to do?',
+    })
+}

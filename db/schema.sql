7d3e8365a36b5a82205deb09393da61ec4518b70
Hbrown16 <hbrown1094@gmail.com>
2021-09-09 19:22:06 -0400

    TABLE EMP

--- db/schema.sql
+++ db/schema.sql
@@ -26,5 +26,24 @@ CREATE TABLE employeeRole (
     CONSTRAINT FK_department_id FORGEIN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-    ON UPDATE CASECADE
+    ON UPDATE CASCADE
+);
+
+-- Create employee table 
+-- child table of employee role
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    lats_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_role_id FORGEIN KEY (role_id)
+    REFERENCES employeeRole(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+    CONSTRAINT FK_manager_id FOREIGN KEY (manager_id)
+    REFERENCES employeeRole(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );

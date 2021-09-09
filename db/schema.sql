83408f6b23736b9b7f9bce7f02076a9a9c8c085d
Hbrown16 <hbrown1094@gmail.com>
2021-09-09 18:49:09 -0400

    tabl dep

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,9 @@ CREATE DATABASE employee_db;
 -- USE DATABASE
 USE employee_db;
 
+-- Creates table for department
+CREATE TABLE department (
+    id INTERGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);

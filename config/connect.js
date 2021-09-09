27de5991546eb050c9073409c293a6e3971ecdc0
Hbrown16 <hbrown1094@gmail.com>
2021-09-09 19:44:25 -0400

    .env

--- config/connect.js
+++ config/connect.js
@@ -1,3 +1,16 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: process.env.user,
+
+    password: process.env.password,
+    database: process.env.database,
+});
+
+module.exports = mysql;
+module.exports = connect;
